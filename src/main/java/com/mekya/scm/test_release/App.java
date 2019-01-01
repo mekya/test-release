5f146f03488cd218d3c0116ea6ba8778f044da88
mekya <ahmetmermerkaya@gmail.com>
2019-01-01 13:32:55 +0300

    first commit

---
+++ src/main/java/com/mekya/scm/test_release/App.java
@@ -0,0 +1,13 @@
+package com.mekya.scm.test_release;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
